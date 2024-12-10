9ca6868bb9e43aa29e53a2dcc11b9fc10db2f6e5
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2024-12-10 12:49:33 +0100

    Add project files.

---
+++ FundamentosNetCore/Form1.cs
@@ -0,0 +1,24 @@
+namespace FundamentosNetCore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Algo aqui...";
+            this.SuspendLayout();
+        }
+
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            this.txtNombre.Location = new Point(20, 10);
+            this.txtNombre.Text = "Soy un string";
+            this.txtNombre.Width = 220;
+            this.txtNombre.TextAlign = HorizontalAlignment.Right;
+            this.btnPulsar.BackColor = Color.Fuchsia;
+        }
+
+       
+    }
+}
